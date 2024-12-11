00b6335af243f330681a81bd5ca1aad4e19d1e45
Rizqi Cahya Angelita <90441810+rizqicahyaa@users.noreply.github.com>
2024-12-11 08:49:51 +0700

    Delete services directory

--- services/storeData.js
+++
@@ -1,9 +0,0 @@
-const { Firestore } = require('@google-cloud/firestore');
-const firestore = new Firestore();
-
-async function storePrediction({ id, result, suggestion, createdAt }) {
-    const docRef = firestore.collection('predictions').doc(id);
-    await docRef.set({ id, result, suggestion, createdAt });
-}
-
-module.exports = { storePrediction };
